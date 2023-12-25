959d455cfa60073c61edf2b29ebd425648838df9
Maciek <maciej.k.glownia@gmail.com>
2023-12-25 23:21:54 +0100

    Add first pipies to create an obstacle for the bird

--- src/index.js
+++ src/index.js
@@ -20,23 +20,26 @@ const config = {
 }
 
 // Loading assets, such as images, music, animations ...
+const VELOCITY = 200;
+
+let bird = null;
+
+let upperPipe = null;
+let lowerPipe = null;
+// debugger
+let pipeVerticalDistanceRange = [150, 250];
+let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
+const flapVelocity = 250;
+const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 }
+
 function preload() {
   // 'this' context - scene
   // contains functions and properties we can use
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
-  this.load.image('upperPipe', 'assets/pipe.png');
-  this.load.image('lowerPipe', 'assets/pipe.png');
+  this.load.image('pipe', 'assets/pipe.png');
 }
-
-const VELOCITY = 200;
-
-let bird = null;
-let upperPipe = null;
-let lowerPipe = null;
-const flapVelocity = 250;
-const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 }
-// const initialPipePosition = { x: config.width * 0.4, y: config.height / 4 }
 
 // Initializing your app
 function create() {
@@ -46,9 +49,11 @@ function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
   // bird = this.physics.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0);
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0);
-  bird.body.gravity.y = 400; 
-  upperPipe = this.physics.add.sprite(400, 100, 'upperPipe').setOrigin(0,1);
-  lowerPipe = this.physics.add.sprite(400, upperPipe.y + 100, 'lowerPipe').setOrigin(0,0);
+  bird.body.gravity.y = 400;
+
+  // debugger
+  upperPipe = this.physics.add.sprite(400, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
+  lowerPipe = this.physics.add.sprite(400, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
 
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown_SPACE', flap);
