dbd982e0677cfbf9308366151f0824e35c818795
Maciek <maciej.k.glownia@gmail.com>
2023-12-25 01:04:45 +0100

    Add upper and lower pipe

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const config = {
     // Arcade phisics plugin, manages phisics symulations
     default: 'arcade',
     arcade: {
-      gravity: { y: 400 },
       debug: true,
     }
   },
@@ -26,14 +25,18 @@ function preload() {
   // contains functions and properties we can use
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
+  this.load.image('upperPipe', 'assets/pipe.png');
+  this.load.image('lowerPipe', 'assets/pipe.png');
 }
 
 const VELOCITY = 200;
 
 let bird = null;
-
+let upperPipe = null;
+let lowerPipe = null;
 const flapVelocity = 250;
 const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 }
+// const initialPipePosition = { x: config.width * 0.4, y: config.height / 4 }
 
 // Initializing your app
 function create() {
@@ -43,6 +46,9 @@ function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
   // bird = this.physics.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0);
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0);
+  bird.body.gravity.y = 400; 
+  upperPipe = this.physics.add.sprite(400, 100, 'upperPipe').setOrigin(0,1);
+  lowerPipe = this.physics.add.sprite(400, upperPipe.y + 100, 'lowerPipe').setOrigin(0,0);
 
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown_SPACE', flap);
