a63ad174a2a442c0e155799359c2123adc3d0e04
Maciek <maciej.k.glownia@gmail.com>
2023-12-26 00:46:05 +0100

    Recycle pipe to loop the game and can play all time

--- src/index.js
+++ src/index.js
@@ -19,39 +19,40 @@ const config = {
   }
 }
 
-// Loading assets, such as images, music, animations ...
 const VELOCITY = 200;
 const PIPES_TO_RENDER = 4;
 
 let bird = null;
+let pipes = null;
+
 let pipeHorizontalDistance = 0;
 let pipeVerticalDistanceRange = [150, 250];
+let pipeHorizontalDistanceRange = [500, 550];
 
 const flapVelocity = 250;
 const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 }
 
 function preload() {
-  // 'this' context - scene
-  // contains functions and properties we can use
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
   this.load.image('pipe', 'assets/pipe.png');
 }
 
-// Initializing your app
 function create() {
-  // key of the image
   this.add.image(0, 0, 'sky').setOrigin(0);
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0);
   bird.body.gravity.y = 400;
 
-  for (let i = 0; i < PIPES_TO_RENDER; i++) {
-    const upperPipe = this.physics.add.sprite(0,0, 'pipe').setOrigin(0, 1);
-    const lowerPipe = this.physics.add.sprite(0,0, 'pipe').setOrigin(0, 0);
+  pipes = this.physics.add.group();
 
-    placePipe(upperPipe, lowerPipe)
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+    const upperPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 1);
+    const lowerPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 0);
+
+    placePipe(upperPipe, lowerPipe);
   }
 
+  pipes.setVelocityX(-200);
 
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown_SPACE', flap);
@@ -59,29 +60,51 @@ function create() {
 
 function update(time, delta) {
   if (bird.y < -bird.height || bird.y > config.height) {
-    restartPlayerPosition();
-    flap();
+    restartBirdPosition();
   }
+  recyclesPipes();
 }
 
 function placePipe(uPipe, lPipe) {
-  pipeHorizontalDistance += 400;
-  let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
-  let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
+  const rightMostX = getRightMostPipe();
+  const pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+  const pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
+  const pipeHorizontalDistance = Phaser.Math.Between(...pipeHorizontalDistanceRange);
 
-  uPipe.x = pipeHorizontalDistance;
+
+  uPipe.x = rightMostX + pipeHorizontalDistance;
   uPipe.y = pipeVerticalPosition;
-  
+
   lPipe.x = uPipe.x;
   lPipe.y = uPipe.y + pipeVerticalDistance;
-  
-  lPipe.body.velocity.x = -200;
-  uPipe.body.velocity.x = -200;
 }
 
-function restartPlayerPosition() {
+function recyclesPipes() {
+  let tempPipes = [];
+  pipes.getChildren().forEach(pipe => {
+    if (pipe.getBounds().right < 0) {
+      tempPipes.push(pipe);
+      if (tempPipes.length == 2) {
+        placePipe(...tempPipes);
+      }
+    }
+  })
+}
+
+function getRightMostPipe() {
+  let rightMostX = 0;
+
+  pipes.getChildren().forEach(function (pipe) {
+    rightMostX = Math.max(pipe.x, rightMostX);
+  })
+  return rightMostX;
+}
+
+
+function restartBirdPosition() {
   bird.x = initialBirdPosition.x
   bird.y = initialBirdPosition.y
+  flap();
 }
 
 function flap() {
