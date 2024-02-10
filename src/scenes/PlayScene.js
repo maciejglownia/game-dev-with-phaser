063e11c30428292d7fd4b30f8619d96f201513f2
Maciej Głownia <maciej.k.glownia@gmail.com>
2024-02-10 22:06:52 +0100

    Add functionality for game over when bird touches pipe, bound. And also pause game for 1sec and restart it

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -28,6 +28,7 @@ class PlayScene extends Phaser.Scene {
     this.createBG();
     this.createBird();
     this.createPipes();
+    this.createColliders();
     this.handleInputs();
   }
 
@@ -43,29 +44,37 @@ class PlayScene extends Phaser.Scene {
   createBird() {
     this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, 'bird').setOrigin(0);
     this.bird.body.gravity.y = 400;
+    this.bird.setCollideWorldBounds(true);
   }
 
   createPipes() {
     this.pipes = this.physics.add.group();
 
     for (let i = 0; i < PIPES_TO_RENDER; i++) {
-      const upperPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 1);
-      const lowerPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 0);
-
+      const upperPipe = this.pipes.create(0, 0, 'pipe')
+      .setImmovable(true)
+      .setOrigin(0, 1);
+      const lowerPipe = this.pipes.create(0, 0, 'pipe')
+      .setImmovable(true)
+      .setOrigin(0, 0);
+      
       this.placePipe(upperPipe, lowerPipe);
     }
 
     this.pipes.setVelocityX(-200);
   }
 
+  createColliders() {
+    this.physics.add.collider(this.bird, this.pipes, this.gameOver, null, this);
+  }
   handleInputs() {
     this.input.on('pointerdown', this.flap, this);
     this.input.keyboard.on('keydown_SPACE', this.flap, this);
   }
 
   checkGameStatus() {
-    if (this.bird.y > this.config.height || this.bird.y < -this.bird.height) {
-      this.restartBirdPosition();
+    if (this.bird.getBounds().bottom >= this.config.height || this.bird.y <= 0) {
+      this.gameOver();
     }
   }
 
@@ -105,10 +114,12 @@ class PlayScene extends Phaser.Scene {
   }
 
 
-  restartBirdPosition() {
-    this.bird.x = this.config.startPosition.x;
-    this.bird.y = this.config.startPosition.y;
-    this.bird.body.velocity.y = 0;
+  gameOver() {
+    // this.bird.x = this.config.startPosition.x;
+    // this.bird.y = this.config.startPosition.y;
+    // this.bird.body.velocity.y = 0;
+    this.physics.pause();
+    this.bird.setTint(0xff0000);
   }
 
   flap() {
