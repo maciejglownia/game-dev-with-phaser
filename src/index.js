25d067a60e1f82551ac561cdca7a870108ed3c69
Maciej Głownia <maciej.k.glownia@gmail.com>
2024-02-11 01:24:29 +0100

    Add BaseScene class, so other scene classes can extend it

--- src/index.js
+++ src/index.js
@@ -13,6 +13,11 @@ const SHARED_CONFIG = {
   startPosition: BIRD_POSITION
 }
 
+const Scenes = [PreloadScene, MenuScene, PlayScene];
+
+const createScene = (Scene) => new Scene(SHARED_CONFIG);
+const initScenes = () => Scenes.map(createScene);
+
 const config = {
   // webGL (Web graphics library) JS Api for rendering 2D and 3D graphic (part of almost every browser by default)
   type: Phaser.AUTO,
@@ -24,11 +29,7 @@ const config = {
       debug: true,
     }
   },
-  scene: [
-    new PreloadScene(),
-    new MenuScene(SHARED_CONFIG),
-    new PlayScene(SHARED_CONFIG)
-  ]
+  scene: initScenes()
 } 
 
 new Phaser.Game(config);
