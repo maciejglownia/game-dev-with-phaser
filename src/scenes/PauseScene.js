564bc8bd61695dca0f6ded45dc380377d0950c6a
Maciej Głownia <maciej.k.glownia@gmail.com>
2024-02-11 21:13:50 +0100

    Handle Continue and Exit button when leaving PauseScene

---
+++ src/scenes/PauseScene.js
@@ -0,0 +1,47 @@
+
+import BaseScene from './BaseScene';
+
+class PauseScene extends BaseScene {
+
+  constructor(config) {
+    super('PauseScene', config);
+
+    this.menu = [
+      { scene: 'PlayScene', text: 'Continue' },
+      { scene: 'MenuScene', text: 'Exit' },
+    ]
+  }
+
+  create() {
+    super.create();
+
+    this.createMenu(this.menu, this.setupMenuEvents.bind(this));
+  }
+
+  setupMenuEvents(menuItem) {
+    const textGO = menuItem.textGO;
+    textGO.setInteractive();
+
+    textGO.on('pointerover', () => {
+      textGO.setStyle({ fill: '#ff0' });
+    })
+
+    textGO.on('pointerout', () => {
+      textGO.setStyle({ fill: '#fff' });
+    })
+
+    textGO.on('pointerup', () => {
+      if (menuItem.scene && menuItem.text === 'Continue') {
+        // Shutting down the PauseScene and resuming the game
+        this.scene.stop();
+        this.scene.resume(menuItem.scene);
+      } else {
+        // PlayScene, PauseScene and running menu
+        this.scene.stop('PlayScene');
+        this.scene.start(menuItem.scene);
+      }
+    })
+  }
+}
+
+export default PauseScene;
