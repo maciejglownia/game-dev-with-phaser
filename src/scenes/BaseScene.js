00466e2bc4c23d9f2845f452c5d2974ba8aecdb7
Maciej Głownia <maciej.k.glownia@gmail.com>
2024-02-11 13:39:37 +0100

    Handle functionality to hover on the menu items, and play when click on Play item

--- src/scenes/BaseScene.js
+++ src/scenes/BaseScene.js
@@ -1,3 +1,4 @@
+
 import Phaser from 'phaser';
 
 class BaseScene extends Phaser.Scene {
@@ -5,23 +6,24 @@ class BaseScene extends Phaser.Scene {
   constructor(key, config) {
     super(key);
     this.config = config;
-        this.screenCenter = [config.width / 2, config.height / 2];
+    this.screenCenter = [config.width / 2, config.height / 2];
     this.fontSize = 34;
     this.lineHeight = 42;
-    this.fontOptions = {fontSize: `${this.fontSize}px`, fill: '#000'};
+    this.fontOptions = {fontSize: `${this.fontSize}px`, fill: '#fff'};
   }
 
   create() {
     this.add.image(0, 0, 'sky').setOrigin(0);
   }
 
-  createMenu(menu) {
+  createMenu(menu, setupMenuEvents) {
     let lastMenuPositionY = 0;
 
     menu.forEach(menuItem => {
       const menuPosition = [this.screenCenter[0], this.screenCenter[1] + lastMenuPositionY];
-      this.add.text(...menuPosition, menuItem.text, this.fontOptions).setOrigin(0.5, 1);
+      menuItem.textGO = this.add.text(...menuPosition, menuItem.text, this.fontOptions).setOrigin(0.5, 1);
       lastMenuPositionY += this.lineHeight;
+      setupMenuEvents(menuItem);
     })
   }
 }
